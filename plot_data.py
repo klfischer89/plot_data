caef84f8af9b4823c66a8b33efba368275074f4c
klfischer89 <fischer-k4@web.de>
2025-11-28 13:01:58 +0100

    added comments

--- plot_data.py
+++ plot_data.py
@@ -3,12 +3,12 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("./data/diamonds.csv.bz2")
+df = pd.read_csv("./data/diamonds.csv.bz2") # read csv
 
-df = df.sample(500, random_state = 42)
+df = df.sample(500, random_state = 42) # sample of dataframe with 500 datasets with consistent state
 
-print(df.head())
+print(df.head()) # print head of dataframe
 
-sns.set_theme()
-sns.scatterplot(x = df["carat"], y = df["price"])
-plt.show()
+sns.set_theme() # make plot have the seaborn theme
+sns.scatterplot(x = df["carat"], y = df["price"]) # create scatterplot
+plt.show() # show the plo
