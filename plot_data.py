c6c1ada80e1df2035b14cc61e0ecae8856698c4f
klfischer89 <fischer-k4@web.de>
2025-11-28 13:05:53 +0100

    configured plot to show the cuts of diamonds

--- plot_data.py
+++ plot_data.py
@@ -10,5 +10,10 @@ df = df.sample(500, random_state = 42) # sample of dataframe with 500 datasets w
 print(df.head()) # print head of dataframe
 
 sns.set_theme() # make plot have the seaborn theme
-sns.scatterplot(x = df["carat"], y = df["price"]) # create scatterplot
-plt.show() # show the plo
+sns.scatterplot(
+    x = df["carat"], 
+    y = df["price"], 
+    style = df["cut"],
+    style_order = ["Fair", "Good", "Very Good", "Premium", "Ideal"],
+    markers = ["o", "v", "8", "X", "*"]) # create scatterplot
+plt.show() # show the plot
