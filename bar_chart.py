2883ffb7d1a6bb8955a5df2379d7585da8583ce1
klfischer89 <fischer-k4@web.de>
2025-11-28 14:00:30 +0100

    added new barchart showing cut with avarage price with colors

---
+++ bar_chart.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("./data/diamonds.csv.bz2") # read csv
+
+df = df.sample(500, random_state = 42) # sample of dataframe with 500 datasets with consistent state
+
+print(df.head()) # print head of dataframe
+
+# df_color = df.groupby("color").agg(count=("cut", len)) # group by color and count by cut
+
+# sns.set_theme()
+# ax = sns.barplot(x = df_color.index, y = df_color["count"]) # barplot that counts the colors
+
+df_grouped = df\
+    .groupby(["cut", "color"])\
+    .agg(avgP = ("price", "mean"))\
+    .reset_index() # group by cut and color considering the average price, and convert cut and color from index to columns and set a new ascending index
+
+df_color = df.groupby("color").agg(count=("cut", len)) # group by color counted by length of cut
+
+sns.set_theme() # set theme to seaborn
+
+sns.barplot(x = df_grouped["cut"],
+            y = df_grouped["avgP"],
+            hue = df_grouped["color"]) # showing barplot showing cut with avarage price, and colored bars
+plt.show() # show bars
